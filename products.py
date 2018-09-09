745c3ce836c524ed2762dd5aecebf9b873137794
Alex Liu <tianyet1026@gmail.com>
2018-09-10 00:03:48 +0800

    Add total payment function

--- products.py
+++ products.py
@@ -5,8 +5,14 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格:')
-	products.append([name, price])
+	price = int(price)
+	unit = input('你買了多少?')
+	unit = int(unit)
+	total = price * unit
+	products.append([name, price, unit, total])
 print(products)	
-
+sum = 0
 for p in products:
-	print(p[0],'的價格是',p[1])
+	print(p[0],'的價格是',p[1],'買了', p[2], '個,付',p[3],'元')
+	sum += p[3] #計算總付出成本
+print('您共支付', sum, '元')
