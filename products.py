ade17b6799e621f53ba65e3d0d05d2431c7e1988
Alex Liu <tianyet1026@gmail.com>
2018-09-10 22:10:56 +0800

    Add write file function

--- products.py
+++ products.py
@@ -16,3 +16,9 @@ for p in products:
 	print(p[0],'的價格是',p[1],'買了', p[2], '個,付',p[3],'元')
 	sum += p[3] #計算總付出成本
 print('您共支付', sum, '元')
+
+# 'abc' + '123' = 'abc123' (字串合併)
+# 'abc' * 3 = 'abcabcabc'
+with open('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + ',' + str(p[2])+ ','+ str(p[3])+ '\n')
