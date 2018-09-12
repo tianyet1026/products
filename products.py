be5dd333cb5e78c29aa75c2869184d8efd6ec702
Alex Liu <tianyet1026@gmail.com>
2018-09-12 20:43:47 +0800

    Add skip header function

--- products.py
+++ products.py
@@ -1,18 +1,15 @@
+#讀取檔案
 products=[]
 with open('products.csv', 'r') as f:
 #with open('products.csv', 'r', encoding = 'utf-8') as f:
 	for line in f:
+		if '商品名稱, 商品價格' in line:
+			continue
 		name, price, unit, total = line.strip().split(',')
 #		print(s)
 		products.append([name, price, unit, total])
 	print(products)
-
-
-
-
-
-
-
+#讓使用者輸入
 while True:
 	name = input('請輸入商品名稱:(輸完請按"q"離開)')
 	if name == 'q':
@@ -24,16 +21,18 @@ while True:
 	total = price * unit
 	products.append([name, price, unit, total])
 print(products)	
+#印出購買紀錄
 #sum = 0
-#for p in products:
-#	print(p[0],'的價格是',p[1],'買了', p[2], '個,付',p[3],'元')
+for p in products:
+	print(p[0],'的價格是',p[1],'買了', p[2], '個,付',p[3],'元')
 #	sum += p[3] #計算總付出成本
 #print('您共支付', sum, '元')
 
 # 'abc' + '123' = 'abc123' (字串合併)
 # 'abc' * 3 = 'abcabcabc'
+#寫入檔案
 with open('products.csv', 'w') as f:
 #with open('products.csv', 'w', encoding = 'utf-8') as f:
-#	f.write('商品名稱, 商品價格, 商品數量, 總支出\n')
+	f.write('商品名稱, 商品價格, 商品數量, 總支出\n') #寫入標題
 	for p in products:
 		f.write(p[0] + ',' + str(p[1]) + ',' + str(p[2])+ ','+ str(p[3])+ '\n')
